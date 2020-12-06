079d2906c69c52177a3c0fd968528385b1ed5bb0
Alex <rzheusskij99@gmail.com>
2020-12-06 13:37:36 +0300

    create permanent redirection to specialistservice

--- course_work/urls.py
+++ course_work/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('specialistservice/', include('specialistservice.urls')),
+    path('',RedirectView.as_view(url = '/specialistservice/', permanent = True)),
 ]
+
+
