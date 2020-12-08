7e523006574d0ed8285f0fe034c0a730bd6c1a2a
Alex <rzheusskij99@gmail.com>
2020-12-08 20:14:23 +0300

    fix model

--- specialistservice/models.py
+++ specialistservice/models.py
@@ -11,26 +11,19 @@ class City(models.Model):
         return self.name
 
 
-class District(models.Model):
-    """Model representing district of city"""
-    name = models.CharField(max_length=20)
-    city = models.ForeignKey(City, on_delete = models.CASCADE)
-
-    def __str__(self):
-        return self.name
-
-
 class User(AbstractUser):
     """Model representing enhanced user"""
     phone = models.CharField(max_length=20,null=True)
-    adress = models.CharField(max_length=50, help_text='Введите адрес места работы', null=True)
+    street = models.CharField(max_length=50, help_text='Введите адрес места работы', null=True)
+    house = models.IntegerField(null=True)
     latitude = models.FloatField(null=True)
     longtitude = models.FloatField(null=True)
     profile_pic = models.ImageField(null=True)
-    district = models.ForeignKey(District, on_delete=models.CASCADE, null=True)
+    city = models.ForeignKey(City, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
+
 
 class Specialty(models.Model):
     """Model representing specialty"""
@@ -49,12 +42,6 @@ class Specialist(models.Model):
 
     def __str__(self):
         return f'{self.person.first_name} {self.person.last_name}'
-
-
-class VisitToClient(models.Model):
-    """Model representig districts that can be visited by specialist"""
-    district = models.ForeignKey(District, on_delete=models.CASCADE)
-    specialist = models.ForeignKey(Specialist, on_delete=models.CASCADE)
 
 
 class Comment(models.Model):
